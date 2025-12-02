a232255521b112383d593d427b966c8108604238
SachithShilshan <sachith.edu6021@gmail.com>
2025-12-02 20:46:36 +0530

    int

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'    // <- set this to /<repo-name>/
+  base: '/portfolio-shilshan/'    // <- set this to /<repo-name>/
 });
