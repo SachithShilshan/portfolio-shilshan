0a69255ab36af643d34a963c403301e329a25e26
SachithShilshan <sachith.edu6021@gmail.com>
2025-12-03 12:14:22 +0530

    des 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-shilshan/'    // <- set this to /<repo-name>/
+  base: '/'    // <- set this to /<repo-name>/
 });
