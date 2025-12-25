32300eeb62eae1f78f462777a48e5d595ee0c0f7
SachithShilshan <sachith.edu6021@gmail.com>
2025-12-25 11:01:40 +0530

    isss

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import './styles/globals.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+   <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
